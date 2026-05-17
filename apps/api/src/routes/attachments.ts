044dab3d443ce7aa0f78515b3b438bc9197a3a53
Takahashi Yasunari <yasunarj@gmail.com>
2026-05-17 20:06:04 +0900

    add: attachments route POST /upload-url & /

--- apps/api/src/routes/attachments.ts
+++ apps/api/src/routes/attachments.ts
@@ -1,14 +1,153 @@
+import { randomUUID } from "node:crypto";
 import { Hono } from "hono";
+import { z } from "zod";
 import { authMiddleware } from "../middleware/auth";
 import { requireRole } from "../middleware/requireRole";
+import { supabaseAdmin } from "../lib/supabase";
+import { createUploadSignedUrl, getIssueAttachmentKey } from "../lib/s3";
 
 const attachments = new Hono();
 
 attachments.use("*", authMiddleware);
 
-attachments.post("/upload-url", requireRole(["admin", "member"]), async (c) => {
-  
+const uploadUrlSchema = z.object({
+  fileName: z.string().min(1).max(255),
+  contentType: z.enum(["image/jpeg", "image/png", "image/webp"]),
+  sizeBytes: z.number().int().positive().max(5 * 1024 * 1024),
+  // numberは数字であること、intは整数であること、positiveは0よりも大きいこと
+  // 5 * 1024 * 1024 これは 5MB までということ
+});
+
+const createAttachmentSchema = z.object({
+  attachmentId: z.uuid(),
+  s3Key: z.string().min(1),
+  fileName: z.string().min(1).max(255),
+  contentType: z.enum(["image/jpeg", "image/png", "image/webp"]),
+  sizeBytes: z.number().int().positive().max(5 * 1024 * 1024), //5MB
 })
 
+attachments.post("/upload-url", requireRole(["admin", "member"]), async (c) => {
+  const user = c.get("user");
+  const issueId = c.req.param("id");
+
+  if (!issueId) {
+    return c.json({ error: "IssueId not found" }, 400);
+  }
+
+  const json = await c.req.json().catch(() => null);
+
+  const parsed = uploadUrlSchema.safeParse(json);
+
+  if (!parsed.success) {
+    return c.json(
+      {
+        error: "Invalid request body",
+        details: parsed.error.issues[0]?.message,
+      },
+      400,
+    );
+  }
+
+  const { fileName, contentType, sizeBytes } = parsed.data
+
+  const { data: issue, error: issueError } = await supabaseAdmin
+    .from("issues")
+    .select("id")
+    .eq("id", issueId)
+    .maybeSingle();
+  // maybeSingleはもしもデータが存在しなかったらエラーではなくnullを返すようにするための関数
+
+  if (issueError) {
+    return c.json({ error: "Failed to fetch issue" }, 500);
+  }
+
+  if (!issue) {
+    return c.json({ error: "Issue not found" }, 404);
+  }
+
+  const attachmentId = randomUUID();
+
+  const s3Key = getIssueAttachmentKey({
+    issueId,
+    attachmentId,
+    fileName,
+  })
+
+  const uploadUrl = await createUploadSignedUrl({
+    key: s3Key,
+    contentType,
+  });
+
+  return c.json({
+    attachmentId,
+    s3Key,
+    fileName,
+    contentType,
+    sizeBytes,
+    uploadUrl,
+    uploadedBy: user.id,
+  });
+});
+
+attachments.post("/", requireRole(["admin", "member"]), async (c) => {
+  const user = c.get("user");
+  const issueId = c.req.param("id");
+
+  if (!issueId) {
+    return c.json({ error: "IssueId not found" }, 400);
+  }
+
+  const json = await c.req.json().catch(() => null);
+
+  const parsed = createAttachmentSchema.safeParse(json);
+
+  if (!parsed.success) {
+    return c.json({
+      error: "Invalid request body",
+      details: parsed.error.issues[0]?.message,
+    }, 400)
+  }
+
+  const { attachmentId, s3Key, fileName, contentType, sizeBytes } = parsed.data;
+
+  const { data: issue, error: issueError } = await supabaseAdmin
+    .from("issues")
+    .select("id")
+    .eq("id", issueId)
+    .maybeSingle();
+
+  if (issueError) {
+    return c.json({
+      error: "Failed to fetch issue"
+    }, 500);
+  }
+
+  if (!issue) {
+    return c.json({ error: "Issue not found" }, 404);
+  }
+
+  const { data: attachment, error: insertError } = await supabaseAdmin
+  .from("issue_attachments")
+  .insert({
+    id: attachmentId,
+    issue_id: issueId,
+    uploaded_by: user.id,
+    s3_key: s3Key,
+    file_name: fileName,
+    content_type: contentType,
+    size_bytes: sizeBytes,
+  })
+  .select("id, issue_id, uploaded_by, s3_key, file_name, content_type, size_bytes, created_at")
+  .single()
+
+  if (insertError) {
+    return c.json({ error: "Failed to create attachment" }, 500);
+  }
+
+  return c.json({
+    attachment,
+  });
+});
 
 export default attachments;
+
